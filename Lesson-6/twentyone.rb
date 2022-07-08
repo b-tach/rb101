c1c378a642538b88dd2d20fcc1848b254a6bb154
Brady Stanton <bradystanton@fastmail.com>
2022-07-07 17:56:22 -0700

    tweaks

--- Lesson-6/twentyone.rb
+++ Lesson-6/twentyone.rb
@@ -14,10 +14,10 @@ CARD_NAMES = ["Ace",
               "Queen",
               "King"]
 
-CARD_SETS = ["Clubs",    # black
-             "Diamonds", # red
-             "Spades",   # black
-             "Hearts"]   # red
+CARD_SUITES = ["Clubs",    # black
+               "Diamonds", # red
+               "Spades",   # black
+               "Hearts"]   # red
 
 #            Ace, 2, 3, 4, 5, 6, 7, 8, 9, 10,  J,  Q,  K
 CARD_VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
@@ -31,7 +31,7 @@ GFX_SPADE = GFX["spade"].split(/\n/)
 GFX_DIAMOND = GFX["diamond"].split(/\n/)
 GFX_CLUB = GFX["club"].split(/\n/)
 GFX_UNKNOWN = GFX["unknown"].split(/\n/)
-GFX_SETS = [GFX_CLUB, GFX_DIAMOND, GFX_SPADE, GFX_HEART, GFX_UNKNOWN]
+GFX_SUITES = [GFX_CLUB, GFX_DIAMOND, GFX_SPADE, GFX_HEART, GFX_UNKNOWN]
 GFX_ID = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", " "]
 
 def prompt(msg)
@@ -50,7 +50,7 @@ end
 
 def get_card_name(card_num)
   card = parse_card(card_num)
-  "#{CARD_NAMES[card[:name_id]]} of #{CARD_SETS[card[:set_id]]}"
+  "#{CARD_NAMES[card[:name_id]]} of #{CARD_SUITES[card[:set_id]]}"
 end
 
 def get_card_value(card_num)
@@ -58,19 +58,19 @@ def get_card_value(card_num)
   CARD_VALUES[card[:name_id]]
 end
 
-def draw_card!(hand, deck)
+def draw_card!(deck, hand)
   hand << deck.slice!(0)
 end
 
 # forgot to shuffle the deck?
-def draw_card_random!(hand, deck)
+def draw_card_random!(deck, hand)
   card = (0..deck.length - 1).to_a.sample
   hand << deck.slice!(card)
 end
 
 def new_hand(deck)
   hand = []
-  2.times { draw_card!(hand, deck) }
+  2.times { draw_card!(deck, hand) }
   hand
 end
 
@@ -90,9 +90,9 @@ def clear_screen
   system("clear")
 end
 
-def display_hand_gfx_card_symbols(hand, dealer = false)
+def display_hand_gfx_card_symbols(hand, is_dealer_hand? = false)
   hand.each_with_index do |card, index|
-    card_id = if dealer && index > 0
+    card_id = if is_dealer_hand? && index > 0
                 13
               else
                 parse_card(card)[:name_id]
@@ -108,37 +108,37 @@ def display_hand_gfx_card_symbols(hand, dealer = false)
   puts
 end
 
-def display_hand_gfx_set_symbols(hand, dealer = false)
+def display_hand_gfx_set_symbols(hand, is_dealer_hand? = false)
   (0..6).each do |vert_slice|
     (0..hand.length - 1).each do |card_index|
-      card_set_id = if dealer && card_index > 0
+      card_set_id = if is_dealer_hand? && card_index > 0
                       4
                     else
                       parse_card(hand[card_index])[:set_id]
                     end
-      print GFX_SETS[card_set_id][vert_slice] + " "
+      print GFX_SUITES[card_set_id][vert_slice] + " "
     end
     puts
   end
 end
 
-def display_hand_gfx(hand, dealer = false)
+def display_hand_gfx(hand, is_dealer_hand? = false)
   hand.length.times { print "#{GFX_TOP} " }
   puts
-  display_hand_gfx_card_symbols(hand, dealer)
-  display_hand_gfx_set_symbols(hand, dealer)
-  display_hand_gfx_card_symbols(hand, dealer)
+  display_hand_gfx_card_symbols(hand, is_dealer_hand?)
+  display_hand_gfx_set_symbols(hand, is_dealer_hand?)
+  display_hand_gfx_card_symbols(hand, is_dealer_hand?)
   hand.length.times { print "#{GFX_BOTTOM} " }
   puts "\n\n"
 end
 
-def dealer_turn!(hand, deck)
-  while hand_card_values(hand).sum < 17
-    draw_card!(hand, deck)
+def dealer_turn!(deck, dealer_hand)
+  while hand_card_values(dealer_hand).sum < 17
+    draw_card!(deck, dealer_hand)
   end
 end
 
-def player_turn!(player_hand, dealer_hand, deck)
+def player_turn!(deck, dealer_hand, player_hand)
   loop do
     clear_screen
     puts "Dealer's Hand:"
@@ -151,7 +151,7 @@ def player_turn!(player_hand, dealer_hand, deck)
     puts "Draw? (type anything starting with 'n/N' for no)"
     ans = gets.chomp[0].downcase
     break if ans == 'n'
-    draw_card!(player_hand, deck)
+    draw_card!(deck, player_hand)
   end
 end
 
@@ -159,7 +159,7 @@ def busted?(score)
   score > 21
 end
 
-def game_round_outcome(player_hand, dealer_hand)
+def game_round_outcome(dealer_hand, player_hand)
   player_score = hand_card_values(player_hand).sum
   dealer_score = hand_card_values(dealer_hand).sum
   if busted?(player_score)
@@ -179,26 +179,39 @@ def game_round_outcome(player_hand, dealer_hand)
   end
 end
 
-def game_round
-  deck = new_deck.shuffle
-  dealer_hand = new_hand(deck)
-  dealer_turn!(dealer_hand, deck)
-  player_hand = new_hand(deck)
-  player_turn!(player_hand, dealer_hand, deck)
-  clear_screen
+def do_game_turns (deck)
+  dealer_turn!(deck, dealer_hand)
+  player_turn!(deck, dealer_hand, player_hand)
+end
+
+def display_game_summary(dealer_hand, player_hand)
   puts "Summary of game:\nDealer's hand:"
   display_hand_gfx(dealer_hand, false)
   puts "Dealer Score: #{hand_card_values(dealer_hand).sum}\n\nPlayer's hand:"
   display_hand_gfx(player_hand, false)
   puts "Player's Score: #{hand_card_values(player_hand).sum}"
-  puts game_round_outcome(player_hand, dealer_hand)
+  puts game_round_outcome(dealer_hand, player_hand)
 end
 
-def main
+def game_round
+  deck = new_deck.shuffle
+  dealer_hand = new_hand(deck)
+  player_hand = new_hand(deck)
+  do_game_turns(deck, dealer_hand, player_hand)
+  clear_screen
+  display_game_summary(dealer_hand, player_hand)
+end
+
+def main_display_intro
   prompt "Welcome to Twenty One!"
   prompt "See your friendly neighborhood (wo)manpages"
   prompt "for information how to play (man -l twentyone.1)"
+  prompt "Or read through 'twentyone.1.md' for the same information."
+  prompt "It is in plain text and human readable."
   puts
+end
+
+def main
   prompt "Would you like to play a round?"
   ans = gets.chomp.downcase[0]
   return if ans != 'y'
